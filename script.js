03fcb636456d0a1485dd410b3b92f27fe57c79dc
Mateo <mateom.realtor@gmail.com>
2022-05-17 21:44:42 -0400

    Updated Javascript

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // Display Date & Time on Page 
 const date = new Date();
 const displayDate = document.querySelector("#currentDay").innerHTML = date.toUTCString();
-// let task = ;
+// let localstorageArr=[]
+let storedInput = localStorage.getItem(); // How we get somthing from local storage.
+//targetting the input field for input 1
+let btnOneInput = document.querySelector("#inputOne.input1");
+
+
+
 
 
 
@@ -9,33 +15,55 @@ const displayDate = document.querySelector("#currentDay").innerHTML = date.toUTC
 let btnOne = document.querySelector("#btnInput1");
 btnOne.addEventListener("click",btnFunc )
 
-function btnFunc() {
-    event.preventDefault();
-    console.log(btnOneInput.value)
 
-    if (btnOneInput.value == " "){
-               window.alert(" Enter Event Before Saving ")
-           }
-           else if(btnOneInput.value) {
-            console.log("WORKKSSKSKS ")
-           }
+ function btnFunc( ) {
+        event.preventDefault();
+        console.log(btnOneInput.value)
 
-           btnOneInput.value = localStorage.setItem()
-}
+        // let data =  JSON.parse(localStorage.getItem(inputValue))
+
+                    // Validaing inPut
+                if(btnOneInput.value) {
+                console.log("WORKKSSKSKS ")
+
+    }
+            else {
+                window.alert(" Enter Event Before Saving ")
+            }
+                // Setting up the local Storage Settings 
+                let input = btnOneInput.value;
+            localStorage.setItem('inputValue', input)
+    
+            for(let i=0; i < localStorage.length; i++){
+                const key = localStorage.key(i)
+                const value =localStorage.getItem(key);
+                    } 
+    getItem();
+                    
+    }
+
+        // Getting items from Local Storage
+
+    function getItem() {
+        if (inputOne.input1.value === "") {
+            window.alert('0');
+        }
+
+        // else {
+
+        //      (   )
+        // }
+       
+    }
 
 
 
-//targetting the input field for input 1
-let btnOneInput =document.querySelector("#inputOne.input1");
-// btnOneInput.addEventListener("click", checkInput) 
+// localStorage.getItem('InputValue', input)
+ 
+        // let input = btnOneInput.value;
+        //  localStorage.setItem('InputValue', input)
 
-
-     // Validaing inPut
-//  function checkInput() {
-//    if (btnOneInput.value == "Hello"){
-//        console.log("WORKKSSKSKS ")
-//    }
-//    else{
-//        console.log(" Wrongggg ")
-//    }
-//  }
+        // for(let i=0; i < localStorage.length; i++){
+        //     const key = localStorage.key(i)
+        //     const value =localStorage.getItem(key);
+        // } 
